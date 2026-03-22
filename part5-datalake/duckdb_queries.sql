f5b19dfd4a0ad81d1b128f62f2ad1682dcdc8fdc
bhoomisolanki3540-hash <bhoomisolanki3540@gmail.com>
2026-03-22 14:18:46 +0530

    Update SQL queries to use relative paths and aliases

--- part5-datalake/duckdb_queries.sql
+++ part5-datalake/duckdb_queries.sql
@@ -2,12 +2,13 @@
 
 SELECT 
     c.customer_id,
-    c.name,
+    c.name AS customer_name,
     COUNT(o.order_id) AS total_orders
-FROM read_csv_auto('C:/Users/EARTH/OneDrive/Attachments/Desktop/bhoomi BA/assignment 3/duckdb/data set/customers.csv') c
-LEFT JOIN read_json_auto('C:/Users/EARTH/OneDrive/Attachments/Desktop/bhoomi BA/assignment 3/duckdb/data set/orders.json') o
+FROM read_csv_auto('../datasets/customers.csv') c
+LEFT JOIN read_json_auto('../datasets/orders.json') o
 ON c.customer_id = o.customer_id
 GROUP BY c.customer_id, c.name;
+
 
 -- Q2: Find the top 3 customers by total order value
 
@@ -15,23 +16,25 @@ SELECT
     c.customer_id,
     c.name AS customer_name,
     SUM(o.total_amount) AS total_order_value
-FROM read_csv_auto('C:/Users/EARTH/OneDrive/Attachments/Desktop/bhoomi BA/assignment 3/duckdb/data set/customers.csv') c
-JOIN read_json_auto('C:/Users/EARTH/OneDrive/Attachments/Desktop/bhoomi BA/assignment 3/duckdb/data set/orders.json') o
+FROM read_csv_auto('../datasets/customers.csv') c
+JOIN read_json_auto('../datasets/orders.json') o
 ON c.customer_id = o.customer_id
 GROUP BY c.customer_id, c.name
 ORDER BY total_order_value DESC
 LIMIT 3;
 
+
 -- Q3: List all products purchased by customers from Bangalore
 
 SELECT DISTINCT 
     p.product_name
-FROM read_csv_auto('C:/Users/EARTH/OneDrive/Attachments/Desktop/bhoomi BA/assignment 3/duckdb/data set/customers.csv') c
-JOIN read_json_auto('C:/Users/EARTH/OneDrive/Attachments/Desktop/bhoomi BA/assignment 3/duckdb/data set/orders.json') o
+FROM read_csv_auto('../datasets/customers.csv') c
+JOIN read_json_auto('../datasets/orders.json') o
 ON c.customer_id = o.customer_id
-JOIN read_parquet('C:/Users/EARTH/OneDrive/Attachments/Desktop/bhoomi BA/assignment 3/duckdb/data set/products.parquet') p
+JOIN read_parquet('../datasets/products.parquet') p
 ON o.order_id = p.order_id
 WHERE c.city = 'Bangalore';
+
 
 -- Q4: Join all three files to show: customer name, order date, product name, and quantity
 
@@ -40,8 +43,8 @@ SELECT
     o.order_date,
     p.product_name,
     p.quantity
-FROM read_csv_auto('C:/Users/EARTH/OneDrive/Attachments/Desktop/bhoomi BA/assignment 3/duckdb/data set/customers.csv') c
-JOIN read_json_auto('C:/Users/EARTH/OneDrive/Attachments/Desktop/bhoomi BA/assignment 3/duckdb/data set/orders.json') o
+FROM read_csv_auto('../datasets/customers.csv') c
+JOIN read_json_auto('../datasets/orders.json') o
 ON c.customer_id = o.customer_id
-JOIN read_parquet('C:/Users/EARTH/OneDrive/Attachments/Desktop/bhoomi BA/assignment 3/duckdb/data set/products.parquet') p
+JOIN read_parquet('../datasets/products.parquet') p
 ON o.order_id = p.order_id;
